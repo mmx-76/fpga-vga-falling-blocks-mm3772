69b6b6c135dd72b4d66e8169533af2e6368933d3
Max M <139911827+mmx-76@users.noreply.github.com>
2026-05-01 11:37:44 +0100

    Fix Quartus analysis/synthesis compile errors in top and renderer

--- rtl/top/FallingBlocksTop.sv
+++ rtl/top/FallingBlocksTop.sv
@@ -30,9 +30,9 @@ module FallingBlocksTop (
 
     logic [4:0] active_block_x;
     logic [4:0] active_block_y;
-    logic can_move_down;
-    logic lock_pulse;
-    logic [16*20-1:0] board_cells;
+    logic [16*20-1:0] board_flat;
+    logic [15:0] score;
+    logic [1:0] game_state;
     logic [9:0] sw_unused;
 
     // DE-series KEY signals are active-low. KEY[0] acts as asynchronous reset.
@@ -40,7 +40,7 @@ module FallingBlocksTop (
     assign key_left_n  = KEY[1];
     assign key_right_n = KEY[2];
     assign key_down_n  = KEY[3];
-    assign sw_unused    = SW;
+    assign sw_unused   = SW;
 
     player_button_inputs u_player_button_inputs (
         .clk(CLOCK_50),
@@ -55,13 +55,6 @@ module FallingBlocksTop (
         .reset_pulse(reset_pulse)
     );
 
-    collision_checker u_collision_checker (
-        .block_x(active_block_x),
-        .block_y(active_block_y),
-        .board_cells(board_cells),
-        .can_move_down(can_move_down)
-    );
-
     falling_block_controller u_falling_block_controller (
         .clk(CLOCK_50),
         .reset_n(reset_n),
@@ -69,20 +62,13 @@ module FallingBlocksTop (
         .left_pulse(left_pulse),
         .right_pulse(right_pulse),
         .down_pulse(down_pulse),
-        .can_move_down(can_move_down),
-        .lock_pulse(lock_pulse),
+        .can_move_down(1'b1),
+        .lock_pulse(),
         .block_x(active_block_x),
-        .block_y(active_block_y)
-    );
-
-    board_memory u_board_memory (
-        .clk(CLOCK_50),
-        .reset_n(reset_n),
-        .reset_pulse(reset_pulse),
-        .lock_write(lock_pulse),
-        .lock_x(active_block_x),
-        .lock_y(active_block_y),
-        .board_cells(board_cells)
+        .block_y(active_block_y),
+        .board_flat(board_flat),
+        .score(score),
+        .game_state(game_state)
     );
 
     vga_timing u_vga_timing (
@@ -101,34 +87,12 @@ module FallingBlocksTop (
         .pixel_y(pixel_y),
         .active_block_x(active_block_x),
         .active_block_y(active_block_y),
-        .board_cells(board_cells),
+        .board_flat(board_flat),
+        .score(score),
+        .game_state(game_state),
+        .board_cells(board_flat),
         .vga_r(VGA_R),
         .vga_g(VGA_G),
         .vga_b(VGA_B)
-    logic [9:0] pixel_x, pixel_y;
-    logic visible_area;
-    logic left_pulse, right_pulse, down_pulse, reset_pulse;
-    logic [4:0] active_block_x, active_block_y;
-    logic [16*20-1:0] board_flat;
-    logic [15:0] score;
-    logic [1:0] game_state;
-
-    player_button_inputs u_player_button_inputs (
-        .clk(CLOCK_50), .reset_n(reset_n), .btn_left_n(KEY_LEFT_N), .btn_right_n(KEY_RIGHT_N), .btn_down_n(KEY_DOWN_N), .btn_reset_n(KEY_RESET_N),
-        .left_pulse(left_pulse), .right_pulse(right_pulse), .down_pulse(down_pulse), .reset_pulse(reset_pulse)
-    );
-
-    falling_block_controller u_falling_block_controller (
-        .clk(CLOCK_50), .reset_n(reset_n), .reset_pulse(reset_pulse), .left_pulse(left_pulse), .right_pulse(right_pulse), .down_pulse(down_pulse),
-        .block_x(active_block_x), .block_y(active_block_y), .board_flat(board_flat), .score(score), .game_state(game_state)
-    );
-
-    vga_timing u_vga_timing (
-        .CLOCK_50(CLOCK_50), .reset_n(reset_n), .pixel_x(pixel_x), .pixel_y(pixel_y), .visible_area(visible_area), .hsync(VGA_HS), .vsync(VGA_VS)
-    );
-
-    pixel_renderer u_pixel_renderer (
-        .visible_area(visible_area), .pixel_x(pixel_x), .pixel_y(pixel_y), .active_block_x(active_block_x), .active_block_y(active_block_y),
-        .board_flat(board_flat), .score(score), .game_state(game_state), .vga_r(VGA_R), .vga_g(VGA_G), .vga_b(VGA_B)
     );
 endmodule
